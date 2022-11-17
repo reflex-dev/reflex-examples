6b0f7f4a0b3ee69be2ede997540aee57c40c8a6a
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2022-11-16 16:28:39 -0800

    Added counter.

---
+++ counter/pcconfig.py
@@ -0,0 +1,8 @@
+# The Pynecone configuration file.
+
+APP_NAME = "counter"
+API_HOST = "http://localhost:8000"
+USERNAME = "pynetree"
+BUN_PATH = "$HOME/.bun/bin/bun"
+ENV = "dev"
+DB_URI = "sqlite:///pynecone.db"
