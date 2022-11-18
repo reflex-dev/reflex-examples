8eb3d2671fba991a6bd5097dfd73dfd06da8f0a5
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2022-11-18 13:32:24 -0800

    Added precommit hooks.

--- nba/pcconfig.py
+++ nba/pcconfig.py
@@ -2,8 +2,7 @@
 
 APP_NAME = "nba"
 API_HOST = "http://localhost:8000"
-USERNAME = "pynetreex"
+USERNAME = ""
 BUN_PATH = "$HOME/.bun/bin/bun"
 ENV = "dev"
 DB_URI = "sqlite:///pynecone.db"
-DEPLOY_URI = "https://0v8g446r99.execute-api.us-west-1.amazonaws.com/prod/hosting-api"
