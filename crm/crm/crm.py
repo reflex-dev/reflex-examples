fb1adc195fa6365e3a50faec6b998e0e498f9b03
Kabir Goel <kabirgoel.kg@gmail.com>
2023-01-26 17:19:22 -0800

    CRM WIP

---
+++ crm/crm/crm.py
@@ -0,0 +1,41 @@
+"""Welcome to Pynecone! This file outlines the steps to create a basic app."""
+from pcconfig import config
+
+import pynecone as pc
+
+docs_url = "https://pynecone.io/docs/getting-started/introduction"
+filename = f"{config.app_name}/{config.app_name}.py"
+
+
+class State(pc.State):
+    """The app state."""
+
+    pass
+
+
+def index():
+    return pc.center(
+        pc.vstack(
+            pc.heading("Welcome to Pynecone!", font_size="2em"),
+            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
+            pc.link(
+                "Check out our docs!",
+                href=docs_url,
+                border="0.1em solid",
+                padding="0.5em",
+                border_radius="0.5em",
+                _hover={
+                    "color": "rgb(107,99,246)",
+                },
+            ),
+            spacing="1.5em",
+            font_size="2em",
+        ),
+        padding_top="10%",
+    )
+
+
+# Add state and page to the app.
+app = pc.App(state=State)
+app.add_page(index)
+app.compile()
