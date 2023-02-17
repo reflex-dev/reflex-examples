a101602b651a6b86f2f9946c7e82af9eca0fce9e
Kabir Goel <kabirgoel.kg@gmail.com>
2023-02-17 01:18:29 -0800

    Remove extraneous stuff from branch

--- eventchains/pcconfig.py
+++
@@ -1,8 +0,0 @@
-import pynecone as pc
-
-
-config = pc.Config(
-    app_name="eventchains",
-    db_url="sqlite:///pynecone.db",
-    env=pc.Env.DEV,
-)
--- tailwind/pcconfig.py
+++
@@ -1,8 +0,0 @@
-import pynecone as pc
-
-
-config = pc.Config(
-    app_name="tailwind",
-    db_url="sqlite:///pynecone.db",
-    env=pc.Env.DEV,
-)
