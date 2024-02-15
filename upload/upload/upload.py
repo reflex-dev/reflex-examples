5f41a7ab6f23846c1d636df5d5d5e3afb52bda53
Masen Furer <m_github@0x26.net>
2024-02-15 13:56:33 -0800

    Support more upload features in the example
    * directory uploads
    * cancellation
    * show selected files

--- upload/upload/upload.py
+++ upload/upload/upload.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from typing import List
 
 import reflex as rx
@@ -16,16 +17,20 @@ class State(rx.State):
     @rx.var
     def files(self) -> list[str]:
         """Get the string representation of the uploaded files."""
-        return os.listdir(rx.get_upload_dir())
+        return [
+            "/".join(p.parts[1:])
+            for p in Path(rx.get_upload_dir()).rglob("*")
+            if p.is_file()
+        ]
 
     async def handle_upload(self, files: List[rx.UploadFile]):
         """Handle the file upload."""
         # Iterate through the uploaded files.
         for file in files:
             upload_data = await file.read()
-            outfile = os.path.join(rx.get_upload_dir(), file.filename)
-            with open(outfile, "wb") as file_object:
-                file_object.write(upload_data)
+            outfile = Path(rx.get_upload_dir()) / file.filename.lstrip("/")
+            outfile.parent.mkdir(parents=True, exist_ok=True)
+            outfile.write_bytes(upload_data)
 
     def on_upload_progress(self, prog: dict):
         print("Got progress", prog)
@@ -35,45 +40,68 @@ class State(rx.State):
             self.is_uploading = False
         self.upload_progress = round(prog["progress"] * 100)
 
+    def cancel_upload(self, upload_id: str):
+        self.is_uploading = False
+        return rx.cancel_upload(upload_id)
+
 
 color = "rgb(107,99,246)"
+upload_id = "upload1"
 
 
 def index():
     return rx.vstack(
         rx.upload(
-            rx.button(
-                "Select File(s)",
-                height="70px",
-                width="200px",
-                color=color,
-                bg="white",
-                border=f"1px solid {color}",
+            rx.vstack(
+                rx.button(
+                    "Select File(s)",
+                    height="70px",
+                    width="200px",
+                    color=color,
+                    bg="white",
+                    border=f"1px solid {color}",
+                ),
+                rx.text(
+                    "Drag and drop files here or click to select files",
+                    height="100px",
+                    width="200px",
+                ),
+                rx.cond(
+                    rx.selected_files(upload_id),
+                    rx.foreach(
+                        rx.selected_files(upload_id),
+                        rx.text,
+                    ),
+                    rx.text("No files selected"),
+                ),
+                align="center",
             ),
-            rx.text(
-                "Drag and drop files here or click to select files",
-                height="100px",
-                width="200px",
-            ),
+            id=upload_id,
             border="1px dotted black",
             padding="2em",
         ),
         rx.hstack(
             rx.button(
                 "Upload",
-                on_click=State.handle_upload(rx.upload_files(on_upload_progress=State.on_upload_progress)),
+                on_click=State.handle_upload(
+                    rx.upload_files(
+                        upload_id=upload_id,
+                        on_upload_progress=State.on_upload_progress
+                    )
+                ),
             ),
         ),
         rx.heading("Files:"),
 
         rx.cond(
             State.is_uploading,
-            rx.progress(value=State.upload_progress),
-            rx.progress(value=0),
+            rx.text("Uploading... ", rx.link("cancel", on_click=State.cancel_upload(upload_id))),
         ),
+        rx.progress(value=State.upload_progress),
         rx.vstack(
            rx.foreach(State.files, lambda file: rx.link(file, href=rx.get_upload_url(file)))
         ),
+        align="center",
     )
 
 
