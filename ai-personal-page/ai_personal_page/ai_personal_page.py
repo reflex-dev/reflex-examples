ce748db7fa9b683aea0367bf37d77af152787ed4
Tom Gotsman <tomgotsman@toms-mbp.lan>
2024-03-27 14:19:04 -0700

    update with cond

--- ai-personal-page/ai_personal_page/ai_personal_page.py
+++ ai-personal-page/ai_personal_page/ai_personal_page.py
@@ -8,12 +8,15 @@ from openai import OpenAI
 
 class State(rx.State):
     """The app state."""
+    chat_bool = False
+
+    def chat_ai(self):
+        self.chat_bool = not self.chat_bool
 
 
 text_size = "5"
 
 _client = None
-
 
 class QA(rx.Base):
     """A question and answer pair."""
@@ -74,10 +77,14 @@ async def process(chat, question: str):
 
         if keep_retrieving_run.status == "completed":
             break
+        
+        if keep_retrieving_run.status == "failed":
+                chat.processing = False
+                yield rx.window_alert("OpenAI Request Failed! Try asking again.")
+                return
 
     # Retrieve messages added by the Assistant to the thread
     all_messages = get_openai_client().beta.threads.messages.list(thread_id=thread.id)
-    print(all_messages)
     answer_text = all_messages.data[0].content[0].text.value
     chat.messages.append({"role": "assistant", "content": answer_text})
     yield
@@ -98,109 +105,102 @@ def index() -> rx.Component:
                 rx.color_mode.switch(),
                 width="100%",
             ),
-            rx.hstack(
-                rx.vstack(
-                    rx.text(
-                        "Welcome to Reflex! Providing insights about Reflex, think about innovating in python. Here are some quick links:",
-                        size=text_size,
-                    ),
-                    rx.unordered_list(
-                        rx.list_item(
-                            rx.flex(
-                                rx.text("Website:", weight="bold"),
+            rx.cond(
+                State.chat_bool,
+                chat(process=process),
+                rx.hstack(
+                    rx.vstack(
+                        rx.text(
+                            "Welcome to Reflex! Providing insights about Reflex, think about innovating in python. Here are some quick links:",
+                            size=text_size,
+                        ),
+                        rx.unordered_list(
+                            rx.list_item(
+                                rx.text("Website: ", weight="bold"),
                                 rx.link(
                                     "🔗 Reflex Site",
                                     href="https:reflex.dev",
                                 ),
-                                spacing="2",
+                                    
                             ),
-                        ),
-                        rx.list_item(
-                            rx.flex(
-                                rx.text("Twitter:", weight="bold"),
+                            rx.list_item(
+                                rx.text("Twitter: ", weight="bold"),
                                 rx.link(
                                     "🔗 @getreflex",
                                     href="https://twitter.com/getreflex",
                                 ),
-                                spacing="2",
+                                    
                             ),
-                        ),
-                        rx.list_item(
-                            rx.flex(
-                                rx.text("Github:", weight="bold"),
+                            rx.list_item(
+                                rx.text("Github: ", weight="bold"),
                                 rx.link(
                                     "🔗 reflex-dev / reflex",
                                     href="https://github.com/reflex-dev/reflex",
-                                ),
-                                spacing="2",
+                                ),    
                             ),
+                            spacing="2",
                         ),
-                        spacing="3",
-                    ),
-                    rx.text(
-                        "Discover the power of Reflex with these commands:",
-                    ),
-                    rx.unordered_list(
-                        rx.list_item(
-                            rx.code("/resume"),
+                        rx.text(
+                            "Discover the power of Reflex with these commands:",
                         ),
-                        rx.list_item(
-                            rx.code("/facts"),
+                        rx.unordered_list(
+                            rx.list_item(
+                                rx.code("/tell me about Reflex"),
+                            ),
+                            rx.list_item(
+                                rx.code("/how do I install Reflex"),
+                            ),
+                            spacing="2",
                         ),
-                        spacing="3",
-                    ),
-                    rx.text(
-                        "Ask a question about Reflex or explore some of the projects below:"
-                    ),
-                    rx.unordered_list(
-                        rx.list_item(
-                            rx.flex(
+                        rx.text(
+                            "Ask a personal AI assistant a question about Reflex or explore some of the projects below:"
+                        ),
+                        rx.unordered_list(
+                            rx.list_item(
                                 rx.link(
                                     "🔗 Reflex Site",
                                     href="https:reflex.dev",
                                 ),
-                                spacing="2",
                             ),
-                        ),
-                        rx.list_item(
-                            rx.flex(
+                            rx.list_item(
                                 rx.link(
                                     "🔗 @getreflex",
                                     href="https://twitter.com/getreflex",
                                 ),
-                                spacing="2",
                             ),
-                        ),
-                        rx.list_item(
-                            rx.flex(
+                            rx.list_item(
                                 rx.link(
                                     "🔗 reflex-dev / reflex",
                                     href="https://github.com/reflex-dev/reflex",
                                 ),
-                                spacing="2",
                             ),
+                            spacing="2",
                         ),
-                        spacing="3",
+                        width="65%",
                     ),
-                    width="65%",
-                ),
-                rx.container(
-                    rx.color_mode_cond(
-                        rx.image(src="/logos/light/reflex.svg", height="4em"),
-                        rx.image(src="/logos/dark/reflex.svg", height="4em"),
+                    rx.container(
+                        rx.color_mode_cond(
+                            rx.image(src="/logos/light/reflex.svg", height="4em"),
+                            rx.image(src="/logos/dark/reflex.svg", height="4em"),
+                        ),
+                        width="35%",
                     ),
-                    width="35%",
+                    spacing="7",
                 ),
-                spacing="7",
             ),
-            chat(process=process),
+            rx.button("Chat with AI", on_click=State.chat_ai, size="4", margin_top="12px"),
             rx.text("Powered by Reflex", size="3"),
             width="70em",
             background_color=rx.color("mauve", 1),
             padding="2em",
             align="center",
             border_radius="1em",
+            margin_top="52px",
+            margin_bottom="24px",
+            margin_x="52px"
+            
         ),
+        
         width="100%",
         height="100vh",
         background="radial-gradient(circle at 22% 11%, rgba(62, 180, 137, .40), hsla(0, 0%, 100%, 0) 19%), radial-gradient(circle at 82% 25%, rgba(33, 150, 243, .36), hsla(0, 0%, 100%, 0) 35%), radial-gradient(circle at 25% 61%, rgba(250, 128, 114, .56), hsla(0, 0%, 100%, 0) 55%)",
@@ -213,9 +213,9 @@ app = rx.App(
         appearance="dark", has_background=True, radius="medium", accent_color="mint"
     ),
     style={
-        rx.text: {"font_size": "20px"},
-        rx.link: {"font_size": "20px"},
-        rx.code: {"font_size": "20px"},
+        rx.text: {"font_size": "15px"},
+        rx.link: {"font_size": "15px"},
+        rx.code: {"font_size": "15px"},
     },
 )
 app.add_page(index)
