abb8c56f2ec017394560f52cc17fcc1621ffd8d7
Tom Gotsman <tomgotsman@toms-mbp.lan>
2024-05-30 14:15:29 -0700

    clean the functionality of the app

--- customer_data_app/customer_data_app/customer_data_app.py
+++ customer_data_app/customer_data_app/customer_data_app.py
@@ -1,6 +1,6 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
-from sqlmodel import select
+from sqlmodel import select, asc, or_, func
 
 import reflex as rx
 
@@ -17,78 +17,84 @@ class Customer(rx.Model, table=True):
 class State(rx.State):
     """The app state."""
 
-    id: int
-    name: str = ""
-    email: str = ""
-    phone: str = ""
-    address: str = ""
     users: list[Customer] = []
     sort_value: str = ""
-    num_customers: int
+    search_value: str = ""
+    num_users: int
+    current_user: Customer = Customer()
+
+    def handle_add_submit(self, form_data: dict):
+        """Handle the form submit."""
+        self.current_user = form_data
+
+    def handle_update_submit(self, form_data: dict):
+        """Handle the form submit."""
+        self.current_user.update(form_data)
 
     def load_entries(self) -> list[Customer]:
-        """Get all users from the database."""
+        """Get all items from the database."""
         with rx.session() as session:
-            self.users = session.exec(select(Customer)).all()
-            self.num_customers = len(self.users)
+            query = select(Customer)
 
-            if self.sort_value:
-                self.users = sorted(
-                    self.users, key=lambda user: getattr(user, self.sort_value).lower()
+            if self.search_value != "":
+                search_value = f"%{self.search_value.lower()}%"
+                query = query.where(
+                    or_(
+                        *[
+                            getattr(Customer, field).ilike(search_value)
+                            for field in Customer.get_fields()
+                        ],
+                    )
                 )
+
+            if self.sort_value != "":
+                sort_column = getattr(Customer, self.sort_value)
+                order = asc(func.lower(sort_column))
+                query = query.order_by(order)
+
+            self.users = session.exec(query).all()
+            self.num_users = len(self.users)
 
     def sort_values(self, sort_value: str):
         self.sort_value = sort_value
         self.load_entries()
 
-    def set_user_vars(self, user: Customer):
-        print(user)
-        self.id = user["id"]
-        self.name = user["name"]
-        self.email = user["email"]
-        self.phone = user["phone"]
-        self.address = user["address"]
+    def filter_values(self, search_value):
+        self.search_value = search_value
+        self.load_entries()
+
+    def get_user(self, user: Customer):
+        self.current_user = user
 
     def add_customer(self):
         """Add a customer to the database."""
         with rx.session() as session:
             if session.exec(
-                select(Customer).where(Customer.email == self.email)
+                select(Customer).where(Customer.email == self.current_user["email"])
             ).first():
-                return rx.window_alert("User already exists")
-            session.add(
-                Customer(
-                    name=self.name,
-                    email=self.email,
-                    phone=self.phone,
-                    address=self.address,
-                )
-            )
+                return rx.window_alert("User with this email already exists")
+            session.add(Customer(**self.current_user))
             session.commit()
         self.load_entries()
-        return rx.window_alert(f"User {self.name} has been added.")
+        return rx.window_alert(f"User {self.current_user["name"]} has been added.")
 
     def update_customer(self):
         """Update a customer in the database."""
         with rx.session() as session:
             customer = session.exec(
-                select(Customer).where(Customer.id == self.id)
+                select(Customer).where(Customer.id == self.current_user["id"])
             ).first()
-            customer.name = self.name
-            customer.email = self.email
-            customer.phone = self.phone
-            customer.address = self.address
-            print(customer)
+            for field in Customer.get_fields():
+                if field != "id":
+                    setattr(customer, field, self.current_user[field])
             session.add(customer)
             session.commit()
         self.load_entries()
 
-    def delete_customer(self, email: str):
+    def delete_customer(self, id: int):
         """Delete a customer from the database."""
         with rx.session() as session:
-            customer = session.exec(
-                select(Customer).where(Customer.email == email)
-            ).first()
+            customer = session.exec(select(Customer).where(Customer.id == id)).first()
             session.delete(customer)
             session.commit()
         self.load_entries()
@@ -97,25 +103,40 @@ class State(rx.State):
         self.load_entries()
 
 
-def show_customer(user: Customer):
-    """Show a customer in a table row."""
-    return rx.table.row(
-        rx.table.cell(rx.avatar(fallback="DA")),
-        rx.table.cell(user.name),
-        rx.table.cell(user.email),
-        rx.table.cell(user.phone),
-        rx.table.cell(user.address),
-        rx.table.cell(
-            update_customer(user),
+def add_fields(field):
+    return rx.flex(
+        rx.text(
+            field,
+            as_="div",
+            size="2",
+            mb="1",
+            weight="bold",
         ),
-        rx.table.cell(
-            rx.button(
-                "Delete",
-                on_click=lambda: State.delete_customer(user.email),
-                bg="red",
-                color="white",
-            ),
+        rx.input(
+            placeholder=field,
+            name=field,
         ),
+        direction="column",
+        spacing="2",
+    )
+
+
+def update_fields_and_attrs(field, attr):
+    return rx.flex(
+        rx.text(
+            field,
+            as_="div",
+            size="2",
+            mb="1",
+            weight="bold",
+        ),
+        rx.input(
+            placeholder=attr,
+            name=field,
+            default_value=attr,
+        ),
+        direction="column",
+        spacing="2",
     )
 
 
@@ -143,41 +164,25 @@ def add_customer():
                 mb="4",
                 padding_bottom="1em",
             ),
-            rx.flex(
-                rx.text(
-                    "Name",
-                    as_="div",
-                    size="2",
-                    mb="1",
-                    weight="bold",
+            rx.form(
+                rx.flex(
+                    # this is a list comprehension that passes each field in the Customer model to the add_fields function unless the field is "id"
+                    *[
+                        add_fields(field)
+                        for field in Customer.get_fields()
+                        if field != "id"
+                    ],
+                    rx.box(
+                        rx.button(
+                            "Submit",
+                            type="submit",
+                        ),
+                    ),
+                    direction="column",
+                    spacing="3",
                 ),
-                rx.input(placeholder="Customer Name", on_blur=State.set_name),
-                rx.text(
-                    "Email",
-                    as_="div",
-                    size="2",
-                    mb="1",
-                    weight="bold",
-                ),
-                rx.input(placeholder="Customer Email", on_blur=State.set_email),
-                rx.text(
-                    "Customer Phone",
-                    as_="div",
-                    size="2",
-                    mb="1",
-                    weight="bold",
-                ),
-                rx.input(placeholder="Input Phone", on_blur=State.set_phone),
-                rx.text(
-                    "Customer Address",
-                    as_="div",
-                    size="2",
-                    mb="1",
-                    weight="bold",
-                ),
-                rx.input(placeholder="Input Address", on_blur=State.set_address),
-                direction="column",
-                spacing="3",
+                on_submit=State.handle_add_submit,
+                reset_on_submit=True,
             ),
             rx.flex(
                 rx.dialog.close(
@@ -215,7 +220,7 @@ def update_customer(user):
                 rx.icon("square_pen", width=24, height=24),
                 bg="red",
                 color="white",
-                on_click=lambda: State.set_user_vars(user),
+                on_click=lambda: State.get_user(user),
             ),
         ),
         rx.dialog.content(
@@ -226,57 +231,26 @@ def update_customer(user):
                 mb="4",
                 padding_bottom="1em",
             ),
-            rx.flex(
-                rx.text(
-                    "Name",
-                    as_="div",
-                    size="2",
-                    mb="1",
-                    weight="bold",
+            rx.form(
+                rx.flex(
+                    *[
+                        update_fields_and_attrs(
+                            field, getattr(State.current_user, field)
+                        )
+                        for field in Customer.get_fields()
+                        if field != "id"
+                    ],
+                    rx.box(
+                        rx.button(
+                            "Submit",
+                            type="submit",
+                        ),
+                    ),
+                    direction="column",
+                    spacing="3",
                 ),
-                rx.input(
-                    placeholder=user.name,
-                    default_value=user.name,
-                    on_blur=State.set_name,
-                ),
-                rx.text(
-                    "Email",
-                    as_="div",
-                    size="2",
-                    mb="1",
-                    weight="bold",
-                ),
-                rx.input(
-                    placeholder=user.email,
-                    default_value=user.email,
-                    on_blur=State.set_email,
-                ),
-                rx.text(
-                    "Customer Phone",
-                    as_="div",
-                    size="2",
-                    mb="1",
-                    weight="bold",
-                ),
-                rx.input(
-                    placeholder=user.phone,
-                    default_value=user.phone,
-                    on_blur=State.set_phone,
-                ),
-                rx.text(
-                    "Customer Address",
-                    as_="div",
-                    size="2",
-                    mb="1",
-                    weight="bold",
-                ),
-                rx.input(
-                    placeholder=user.address,
-                    default_value=user.address,
-                    on_blur=State.set_address,
-                ),
-                direction="column",
-                spacing="3",
+                on_submit=State.handle_update_submit,
+                reset_on_submit=True,
             ),
             rx.flex(
                 rx.dialog.close(
@@ -326,13 +300,36 @@ def navbar():
     )
 
 
+def show_customer(user: Customer):
+    """Show a customer in a table row."""
+    return rx.table.row(
+        rx.table.cell(rx.avatar(fallback="DA")),
+        *[
+            rx.table.cell(getattr(user, field))
+            for field in Customer.get_fields()
+            if field != "id"
+        ],
+        rx.table.cell(
+            update_customer(user),
+        ),
+        rx.table.cell(
+            rx.button(
+                "Delete",
+                on_click=lambda: State.delete_customer(getattr(user, "id")),
+                bg="red",
+                color="white",
+            ),
+        ),
+    )
+
+
 def content():
     return rx.fragment(
         rx.vstack(
             rx.divider(),
             rx.hstack(
                 rx.heading(
-                    f"Total: {State.num_customers} Customers",
+                    f"Total: {State.num_users} Customers",
                     size="5",
                     font_family="Inter",
                 ),
@@ -344,6 +341,10 @@ def content():
                     on_change=lambda sort_value: State.sort_values(sort_value),
                     font_family="Inter",
                 ),
+                rx.input(
+                    placeholder="Search here...",
+                    on_change=lambda value: State.filter_values(value),
+                ),
                 width="100%",
                 padding_x="2em",
                 padding_top="2em",
@@ -352,17 +353,18 @@ def content():
             rx.table.root(
                 rx.table.header(
                     rx.table.row(
-                        rx.table.column_header_cell("Icon"),
-                        rx.table.column_header_cell("Name"),
-                        rx.table.column_header_cell("Email"),
-                        rx.table.column_header_cell("Phone"),
-                        rx.table.column_header_cell("Address"),
-                        rx.table.column_header_cell("Edit"),
-                        rx.table.column_header_cell("Delete"),
+                        rx.table.column_header_cell("icon"),
+                        # this is a list comprehension that passes each field in the Customer model to the column_header_cell function unless the field is "id"
+                        *[
+                            rx.table.column_header_cell(field)
+                            for field in Customer.get_fields()
+                            if field != "id"
+                        ],
+                        rx.table.column_header_cell("edit"),
+                        rx.table.column_header_cell("delete"),
                     ),
                 ),
                 rx.table.body(rx.foreach(State.users, show_customer)),
-                # variant="surface",
                 size="3",
                 width="100%",
             ),
