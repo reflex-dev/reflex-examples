be65b41c76cd8b4f2e9bf7a7206a81ca120c58f5
elvis kahoro <elvis@reflex.dev>
2024-10-22 17:25:32 -0700

    feat: Adding the chat app (#269)
    * feat: Adding the chat app
    
    * feat: Improving otel events to include more of the prompt context
    
    * fix: requirements.txt
    
    * fix: Swapping out from GRPC to HTTP so that it's cross compatible with
    phoenix
    
    * Resetting alembic
    
    * fix: Caching messages so it's only recomputed upon underlying state
    change
    
    * refactor: Moving database query to happen on load instead of within a
    computed var

---
+++ chat_v2/chat_v2/components/buttons.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Callable
+
+import reflex as rx
+
+from .loading_icon import loading_icon
+
+
+@dataclass
+class Style:
+    default: dict[str, str | rx.Component] = field(
+        default_factory=lambda: {
+            "radius": "large",
+            "cursor": "pointer",
+            "variant": "classic",
+            "padding": "18px 16px",
+        },
+    )
+
+
+BUTTON_STYLE: Style = Style()
+
+
+def button_with_icon(
+    text: str,
+    icon: str,
+    on_click: Callable,
+    is_loading: bool = False,
+    **kwargs,
+):
+    """Creates a button with an icon.
+    - text: The text of the button.
+    - icon: The icon tag name.
+    - **kwargs: Additional keyword arguments for rx.button.
+    """
+    return rx.button(
+        rx.cond(
+            is_loading,
+            loading_icon(
+                height="1em",
+            ),
+            rx.hstack(
+                rx.icon(
+                    tag=icon,
+                    size=14,
+                ),
+                rx.text(
+                    text,
+                    size="2",
+                    align="center",
+                    weight="medium",
+                    font_family="Inter",
+                ),
+                gap="8px",
+                width="100%",
+                display="flex",
+                align="center",
+                justify="center",
+            ),
+        ),
+        **BUTTON_STYLE.default,
+        **kwargs,
+        on_click=on_click,
+    )
