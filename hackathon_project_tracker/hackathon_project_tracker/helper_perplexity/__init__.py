943d79655d8f8c0c10ee697595ba3dc7c87a0b9b
elvis kahoro <elvis@reflex.dev>
2024-11-03 11:02:10 -0800

    fix: Fixing the helper perplexity imports

--- hackathon_project_tracker/hackathon_project_tracker/helper_perplexity/__init__.py
+++ hackathon_project_tracker/hackathon_project_tracker/helper_perplexity/__init__.py
@@ -2,3 +2,11 @@ from .helper_perplexity import Client
 from .model_chat_completion import DEFAULT_PROMPT, ChatCompletion, Message
 from .model_chat_response import ChatResponse
 
+__all__ = [
+    "Client",
+    "DEFAULT_PROMPT",
+    "ChatCompletion",
+    "Message",
+    "ChatResponse",
+]
+
