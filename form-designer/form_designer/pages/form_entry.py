c3d89994940cbcfcabce326a9049356d089b08d4
Masen Furer <m_github@0x26.net>
2024-11-18 16:18:38 -0800

    Handle required fields

--- form-designer/form_designer/pages/form_entry.py
+++ form-designer/form_designer/pages/form_entry.py
@@ -36,7 +36,7 @@ class FormEntryState(rx.State):
         )
         for field in self.form.fields:
             value = form_data.get(field.name, Missing)
-            if value is not Missing:
+            if value and value is not Missing:
                 response.field_values.append(
                     FieldValue(
                         field_id=field.id,
@@ -44,13 +44,18 @@ class FormEntryState(rx.State):
                     )
                 )
             elif field.type_ == FieldType.checkbox:
+                field_values = []
                 for option in field.options:
                     key = f"{field.name}___{option.value or option.label or option.id}"
                     value = form_data.get(key, Missing)
                     if value is not Missing:
-                        response.field_values.append(
+                        field_values.append(
                             FieldValue(field_id=field.id, value=form_data[key])
                         )
+                if field.required and not field_values:
+                    return rx.toast(f"Field '{field.name}' is required.")
+            elif field.required:
+                return rx.toast(f"Field '{field.name}' is required.")
         with rx.session() as session:
             session.add(response)
             session.commit()
